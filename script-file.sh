763750c376c36dc49592148e8b580e5229615331
Ali Shehzad <princealishehzad943@gmail.com>
2025-11-01 13:03:53 +0500

    Update script-file.sh

--- script-file.sh
+++ script-file.sh
@@ -15,32 +15,42 @@ cd ~/Docker-files
 #STEP 3: Create Text Files and paste content
 #Docker1 files
 cd Docker1
-echo "Atticus said to Jem one day, I’d rather you shot at tin cans in the backyard, but I know you’ll go after birds.Shoot all the blue jays you want,
+echo "Atticus said to Jem one day, I’d rather you shot at tin cans in the backyard, but I know you’ll go after birds.Shoot all the blue jays you want, 
 if you can hit ‘em, but remember it’s a sin to kill a mockingbird." > Aviana.txt
 echo "That was the only time I ever heard Atticus say it was a sin to do something, and I asked Miss Maudie about it." > Bryant.txt
-echo "Your father’s right, she said. Mockingbirds don’t do one thing except make music for us to enjoy." > Kyron.txt
-echo "They don’t eat up people’s gardens, don’t nest in corn cribs, they don’t do one thing but sing their hearts out for us." > Rowland.txt
+echo "Your father’s right,” she said. “Mockingbirds don’t do one thing except make music for us to enjoy." > Kyron.txt
+echo "They don’t eat up people’s gardens, don’t nest in corn cribs, they don’t do one thing but sing their hearts out for us. " > Rowland.txt
 echo "That’s why it’s a sin to kill a mockingbird." > Suhayb.txt
 
 #  Docker2 files
 cd ../Docker2
-echo "The most important things are the hardest to say. They are the things you get ashamed of, because words diminish them." > Aaisha.txt
+echo "The most important things are the hardest to say. They are the things you get ashamed of, because words diminish them — 
+words shrink things that seemed limitless when they were in your head to no more than living size when they’re brought out." > Aaisha.txt
 echo "But it’s more than that, isn’t it?" > Aleah.txt
-echo "The most important things lie too close to wherever your secret heart is buried." > Arnold.txt
-echo "And you may make revelations that cost you dearly." > Hall.txt
+echo "The most important things lie too close to wherever your secret heart is buried, like landmarks to a treasure your enemies would love to steal away." > Arnold.txt
+echo "And you may make revelations that cost you dearly only to have people look at you in a funny way, not understanding what you’ve said at all, 
+or why you thought it was so important that you almost cried while you were saying it." > Hall.txt
 echo "That’s the worst, I think." > Rhea.txt
 echo "When the secret stays locked within not for want of a teller but for want of an understanding ear." > Skinner.txt
 
 # Docker3 files
 cd ../Docker3
-echo "Sometimes fate is like a small sandstorm that keeps changing directions." > Chan.txt
-echo "Why? Because this storm isn’t something that blew in from far away." > Costa.txt
-echo "So all you can do is give in to it, step right inside the storm." > Cydney.txt
-echo "There’s no sun there, no moon, no direction, no sense of time." > Frederick.txt
-echo "You really will have to make it through that violent, metaphysical storm." > Hampton.txt
-echo "People will bleed there, and you will bleed too." > Jane.txt
-echo "Once the storm is over you won’t remember how you made it through." > Luna.txt
-echo "When you come out of the storm you won’t be the same person who walked in." > Yassin.txt
+echo "Sometimes fate is like a small sandstorm that keeps changing directions. You change direction but the sandstorm chases you. 
+You turn again, but the storm adjusts.Over and over you play this out, like some ominous dance with death just before dawn." > Chan.txt
+echo "Why? Because this storm isn’t something that blew in from far away, 
+something that has nothing to do with you. This storm is you.Something inside of you. " > Costa.txt
+echo "So all you can do is give in to it, step right inside the storm, 
+closing your eyes and plugging up your ears so the sand doesn’t get in, and walk through it, step by step." > Cydney.txt
+echo "There’s no sun there, no moon, no direction, no sense of time. Just fine white sand swirling up into the sky like pulverized bones. 
+That’s the kind of sandstorm you need to imagine." > Frederick.txt
+echo "And you really will have to make it through that violent, metaphysical, symbolic storm. No matter how metaphysical or symbolic it might be,
+make no mistake about it: it will cut through flesh like a thousand razor blades." > Hampton.txt
+echo "People will bleed there, and you will bleed too. Hot, red blood. 
+You’ll catch that blood in your hands, your own blood and the blood of others." > Jane.txt
+echo "And once the storm is over you won’t remember how you made it through, how you managed to survive. 
+You won’t even be sure, in fact, whether the storm is really over." > Luna.txt
+echo "But one thing is certain. When you come out of the storm you won’t be the same person who walked in. 
+That’s what this storm’s all about." > Yassin.txt
 
 #STEP 4: Build Docker Custom Image through Dockerfile
 cd ~/Docker-files
